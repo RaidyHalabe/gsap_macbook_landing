39e8b9ee952b05678096ce90f91d8419ef1d366b
Raidy halabe <163164165+RaidyHalabe@users.noreply.github.com>
2026-03-31 17:15:23 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,11 @@
-import NavBar from "./components/NavBar.jsx";
-import Hero from "./components/Hero.jsx";
-import ProductViewer from "./components/ProductViewer.jsx";
-import gsap from 'gsap';
-import { ScrollTrigger } from "gsap/all";
-import Showcase from "./components/Showcase.jsx";
-import Performance from "./components/Performance.jsx";
-import Features from "./components/Features.jsx";
-import Highlights from "./components/Highlights.jsx";
-import Footer from "./components/Footer.jsx";
-
-gsap.registerPlugin(ScrollTrigger)
+import NavBar from './components/Navbar'
 
 const App = () => {
-    return (
-        <main>
-            <NavBar />
-            <Hero />
-            <ProductViewer />
-            <Showcase />
-            <Performance />
-            <Features />
-            <Highlights />
-            <Footer />
-        </main>
-    )
+  return (
+    <main>
+      <NavBar></NavBar>
+    </main>
+  )
 }
 
 export default App
